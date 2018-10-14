f79cace4b239cd1fdf68689ec0838297b61c48f5
Paweł Zielonka <lite.what@gmail.com>
2018-10-14 22:52:51 +0200

    Removed unused endpoint

--- src/clj/clojure_challenge_gui/routes/home.clj
+++ src/clj/clojure_challenge_gui/routes/home.clj
@@ -2,7 +2,8 @@
   (:require [clojure-challenge-gui.layout :as layout]
             [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.tools.logging :as log]))
 
 (defn home-page []
   (-> (layout/render "home.html")
@@ -11,8 +12,7 @@
 (defroutes home-routes
   (GET "/" []
     (home-page))
+
   (GET "/docs" []
     (-> (response/ok (-> "docs/docs.md" io/resource slurp))
-        (response/header "Content-Type" "text/plain; charset=utf-8")))
-  (POST "/scramble" []
-    (response/ok {:result true})))
+        (response/header "Content-Type" "text/plain; charset=utf-8"))))
